93b197c8a252aab83778bb45f8f624e7fe492e7d
Seetharaman Iyer <benex1114@gmail.com>
2026-01-28 18:45:16 +0530

    Update ProjectRepository.java

--- envmanager/src/main/java/com/remotemanager/envmanager/repository/ProjectRepository.java
+++ envmanager/src/main/java/com/remotemanager/envmanager/repository/ProjectRepository.java
@@ -1,8 +1,12 @@
 package com.remotemanager.envmanager.repository;
  
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.remotemanager.envmanager.model.Project;
  
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findByNameContainingIgnoreCase(String keyword);
 }
